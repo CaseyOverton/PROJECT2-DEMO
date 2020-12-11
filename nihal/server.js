2a07e09ebf73c29096f47de91a854d1b38c70b12
nihalwill <66322270+nihalwill@users.noreply.github.com>
2020-12-10 20:46:14 -0500

    adding in php to work with table generation

--- nihal/server.js
+++ nihal/server.js
@@ -2,6 +2,7 @@
 require("path");
 const express = require("express");
 const db = require("./models");
+const php = require('php')
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -10,8 +11,18 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
 
+
+app.set('views', path.join(__dirname, 'templates'))
+app.set('view engine', 'php')
+app.engine('php', php.__express)
+
+
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app); 
+
+app.get('/table', (req, res) => {
+  res.render('table.php')
+})
 
 db.sequelize.sync({}).then(function() {
     app.listen(PORT, function() {
