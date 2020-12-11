9f3545c8cb41e556e081dedf355bf5e3fa963472
James Baldwin <jamescbaldwin@gmail.com>
2020-12-11 16:09:47 -0500

    yardsale categories variable

---
+++ james/categories.js
@@ -0,0 +1,17 @@
+const yardsaleCat = [
+    "Apparel",
+    "Appliances",
+    "Automotive",
+    "Electronics",
+    "Furniture",
+    "Gaming",
+    "Home Decor",
+    "Jewelry",
+    "Miscellaneous",
+    "Outdoor",
+    "Sporting Goods",
+];
+
+for (let i = 0; i < yardsaleCat.length; i++) {
+    console.log(yardsaleCat[i]);
+};
