3bbaeff6889f0b9f6b5efd9b9a403a54a9735bf6
James Baldwin <jamescbaldwin@gmail.com>
2020-12-12 12:26:13 -0500

    front-end buyer js

--- james/buyers.js
+++ james/buyers.js
@@ -1,3 +1,30 @@
+$(document).ready(() => {
+    const purchaseButton = $("purchase-button");
+    const itemPrice = $(".item-price");
+    const itemName = $(".item-name");
+    const contactInfo = $(".contact-info");
+
+    purchaseButton.on("click", event => {
+        event.preventDefault();
+        const purchase = {
+            itemName: itemName.val().trim(),
+            price: itemPrice.val(),
+            contactInfo: contactInfo.val().trim()
+        };
+
+        $.ajax("/api/purchase/", {
+            type: "PUT",
+            data: purchase
+        }).then(function () {
+            location.reload();
+        },
+
+        alert("For payment information, please contact seller at " + contactInfo);
+    })
+
+})
+
+
 module.exports = function(sequelize, DataTypes) {
     const Buyer = sequelize.define('Buyer', {
        buyer_name: {
