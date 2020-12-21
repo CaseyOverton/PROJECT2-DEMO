cf382e00bb2ddc6632659c542355d6b85522ca20
Cadertea <cadertea@ncsu.edu>
2020-12-21 16:54:18 -0500

    added a delete function that deletes cards from the buyers page as well as the database

--- public/js/buyers.js
+++ public/js/buyers.js
@@ -1,38 +1,57 @@
-$('.btn').on('click', function () {
+
+$('.card-columns').on('click',".btn", function () {
     console.log('button')
 
-    let currentId = $(this).parent().attr("id");
-    currentId = parseInt(currentId);
-    console.log($(this).parent())
+    let currentId = $(this).attr("data-id");
+    // currentId = parseInt(currentId);
+    console.log(currentId)
     $(this).addClass("hide");
     $(this).siblings('.alert').removeClass('hide');
-    
+    // var id = $(this).data("id");
+    $.ajax({
+      method: "DELETE",
+      url: "/api/newseller/" + currentId
+    }).then(function(response){
+      console.log("Delete",response)
+      getTodos();
+    });
 });
+
+
+function getTodos(){
+
 var posts;
 var blogContainer = $(".card-columns");
 
 $.get("/api/newseller", function(data) {
-
+  console.log("Data",data)
     if (data.length !== 0) {
   
       for (var i = 0; i < data.length; i++) {
+        console.log(data[i],"for loop")
   
-        var row = $("<div>");
-        row.addClass("card");
+        let newItem = 
+        `
+              <div class="card">
+              <div class="card-body">
+                  <h5 class="item-name">${data[i].item_name}</h5>
+                  <h6 class="item-price">${data[i].item_price}</h6>
+                  <p class="card-text">${data[i].sellers_bio}</p>
+                  <p class="contact-info"><small class="text-muted">${data[i].sellers_email}></p>
+                <button type="button"  data-id ='${data[i].id}' class="btn btn-outline-success">Purchase</button>
+              </div>
+            </div>
+            `
   
-        row.append("<b>"+"<h5>" + data[i].item_name + "</h5>"+"</b>");
-        row.append("<h6>" + data[i].item_price + "</h6>");
-        row.append("<h5>" + data[i].sellers_bio + "</h5>");
-        row.append("<h6>"+ "Contact " + data[i].seller_name + "<br>" +" at "+  "<br>" + data[i].sellers_email+"</h6>");
-       
-  
-        blogContainer.prepend(row);
+        blogContainer.prepend(newItem );
   
       }
   
     }
   
   });
-  
+}
+
+getTodos()
 
   
